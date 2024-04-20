2cb5fe302eda4398c29a5c2d764e6956ee9b8410
Prudhvi Raj Grandhe <prudhviraj@Prudhvis-MacBook-Air.local>
2024-04-21 02:17:57 +0530

    new

--- app.py
+++ app.py
@@ -61,6 +61,10 @@ def generate_frames():
         
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+@app.route('/')
+def index():
+    return "Hello World"
+
 
 @app.route('/video')
 def video():
